efa2242fe26aedf1ddc10c0792dd310b42e5de77
Ali Ghasemi <70671322+alialaki@users.noreply.github.com>
2024-09-06 12:44:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 FutuJob resume tailoring AI app')
 
 st.info('This is app builds a machine learning model!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/alialaki/data/master/penguins_cleaned.csv')
+
+df
