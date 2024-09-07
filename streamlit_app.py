a354c3a116f92202bbdfad8faa63d557cb23ab44
Ali Ghasemi <70671322+alialaki@users.noreply.github.com>
2024-09-06 17:54:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 import pandas as pd
 import json
+import requests
+
+url = "https://jsearch.p.rapidapi.com/search-filters"
+
+querystring = {"query":"Node.js developer in New-York,USA","date_posted":"all"}
+
+headers = {
+	"x-rapidapi-key": "673b3befb0mshea186ed7da8eb81p1fd395jsnae3d478a96fb",
+	"x-rapidapi-host": "jsearch.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+
 
 st.title('🤖 FutuJob resume tailoring AI app')
 
@@ -9,3 +23,5 @@ st.info('This is app builds a machine learning model!')
 df = pd.read_csv('https://raw.githubusercontent.com/alialaki/data/master/penguins_cleaned.csv')
 df
 
+
+st(response.json())
